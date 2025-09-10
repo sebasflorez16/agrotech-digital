bdb38ecce604e08a405911330268731c0e8380c0
sebasflorez16 <juansebastianflorezescobar@gmail.com>
2025-09-10 16:28:26 -0500

    Add missing allauth.account.middleware.AccountMiddleware
    - Fix ImproperlyConfigured error for django-allauth
    - Add AccountMiddleware after AuthenticationMiddleware
    - Required for allauth account management functionality

--- config/settings/production.py
+++ config/settings/production.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",  
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "allauth.account.middleware.AccountMiddleware",  # Requerido por django-allauth
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
